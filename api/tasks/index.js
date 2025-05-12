9c7fabeac4a4399d778dda0cec938490448f5cbe
Subhadeep Ghosh <subhadeep051@icloud.com>
2025-05-12 16:59:26 +0530

    first commit

--- api/tasks/index.js
+++ api/tasks/index.js
@@ -1,4 +1,4 @@
-import pool from '../../lib/db';
+import pool from '@/lib/db';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') return res.status(405).end('Method Not Allowed');
